015ef1fdf7271f330b1b49a1c4ffc8e706c03fc4
Francois Favreau <francois.favreau@usherbrooke.ca>
2018-08-23 22:51:55 -0400

    Saut de ligne

--- Code_Matlab/ampl_freq.m
+++ Code_Matlab/ampl_freq.m
@@ -1,10 +1,11 @@
 function [ final_output_spike ] = ampl_freq( y, threshold, nb_samples, final_output_spike )
 % Fill the spike matrix corresponding to the signal
+
     tamp = 0;
     for i=1:nb_samples
-       tamp = tamp + 1+y(i);
+        tamp = tamp + y(i);
         if tamp > threshold
-            final_output_spike(:,i,:) = 1; % 0 and 1        
+            final_output_spike(:,i,:) = 1;
             tamp = 0;
         else
             final_output_spike(:,i,:) = 0;
